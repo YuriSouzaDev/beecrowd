c8c311b1985f50ed62fb5106bf409bc03102caf1
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-04-03 07:29:23 -0300

    :sparkles: feat: challenger 1005 solved

--- problems/1005.js
+++ problems/1005.js
@@ -1,18 +1,18 @@
-/* Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.   
+/* Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
 
 Entrada
-O arquivo de entrada contém 2 valores inteiros.
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
 
 Saída
-Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”. */
+Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error". */
 
 export function problem(lines) {
   const [A, B] = lines;
 
-  function calcularProduto(A, B) {
-    const PROD = Number(A) * Number(B);
-    console.log(`PROD = ${PROD}`);
+  function calcularMedia(A, B) {
+    const media = (parseFloat(A) * 3.5 + parseFloat(B) * 7.5) / 11;
+    console.log(`MEDIA = ${media.toFixed(5)}`);
   }
 
-  calcularProduto(A, B);
+  calcularMedia(A, B);
 }
