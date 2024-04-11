791d0948f25d52b0eed45f2ef3a854e4b52bf751
YuriSouzaDev <yurisouza.developer@gmail.com>
2024-04-11 15:28:31 -0300

    :sparkles: feat: challenger 1018 solved

--- problems/1018.js
+++ problems/1018.js
@@ -10,36 +10,13 @@ export function problem(lines) {
   const [N] = lines.map(Number);
 
   const cedulas = [100, 50, 20, 10, 5, 2, 1];
-  let resto = N;
 
   console.log(N);
+  let resto = N;
+
   for (let i = 0; i < cedulas.length; i++) {
     const quantidadeCedulas = Math.floor(resto / cedulas[i]);
     resto %= cedulas[i];
     console.log(`${quantidadeCedulas} nota(s) de R$ ${cedulas[i]},00`);
   }
-
-  // const cedulas100 = Math.floor(N / 100);
-  // resto = N % 100;
-  // const cedulas50 = Math.floor(resto / 50);
-  // resto = resto % 50;
-  // const cedulas20 = Math.floor(resto / 20);
-  // resto = resto % 20;
-  // const cedulas10 = Math.floor(resto / 10);
-  // resto = resto % 10;
-  // const cedulas5 = Math.floor(resto / 5);
-  // resto = resto % 5;
-  // const cedulas2 = Math.floor(resto / 2);
-  // resto = resto % 2;
-  // const cedulas1 = Math.floor(resto / 1);
-  // resto = resto % 1;
-
-  // console.log(N);
-  // console.log(`${cedulas100} nota(s) de R$ 100,00`);
-  // console.log(`${cedulas50} nota(s) de R$ 50,00`);
-  // console.log(`${cedulas20} nota(s) de R$ 20,00`);
-  // console.log(`${cedulas10} nota(s) de R$ 10,00`);
-  // console.log(`${cedulas5} nota(s) de R$ 5,00`);
-  // console.log(`${cedulas2} nota(s) de R$ 2,00`);
-  // console.log(`${cedulas1} nota(s) de R$ 1,00`);
 }
